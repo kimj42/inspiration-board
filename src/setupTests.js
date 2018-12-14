00e42d493ea029a65cacea4cef814eb28c8d6858
Karis Kim <ouniiq@hotmail.com>
2018-12-14 09:55:47 -0800

    added axios package and confirmed it existing in the dependencies section

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,10 @@
-import Enzyme from 'enzyme';
+// import Enzyme from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+//
+// Enzyme.configure({ adapter: new Adapter() });
+
+// src/setupTests.js
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
