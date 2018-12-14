964f8e7454479561530308e5ec2ab6c667c09747
Karis Kim <ouniiq@hotmail.com>
2018-12-14 11:45:35 -0800

    moved around app and board test to original position and created snapshots

---
+++ src/components/Board.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Board from './Board.js';
+import { shallow } from 'enzyme';
+
+describe('Board', () => {
+
+  test('also see if boad works', () => {
+
+  });
+
+});
---
+++ src/components/test/NewCardForm.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import NewCardForm from '../NewCardForm.js';
+import { shallow } from 'enzyme';
+
+describe('NewCardForm', () => {
+  test('see if newcardform works', () => {
+
+  });
+});
