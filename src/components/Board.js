2668b91b1cff6b9a3247295dfb3d7612e629ef5d
Karis Kim <ouniiq@hotmail.com>
2018-12-14 10:29:01 -0800

    able to do a get request for all cards of my board, red panda

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
+// import CARD_DATA from '../data/card-data.json';
 
 class Board extends Component {
   constructor() {
@@ -13,19 +13,33 @@ class Board extends Component {
 
     this.state = {
       cards: [],
+      errors: [],
     };
+  };
 
+  componentDidMount() {
+    const GET_ALL_CARDS = "https://inspiration-board.herokuapp.com/boards/redPanda/cards";
+
+    axios.get(GET_ALL_CARDS)
+    .then((response) => {
+      this.setState({
+        cards: response.data,
+      });
+    })
+    .catch((error) => {
+      this.setState({
+        errors: error.message
+      });
+    });
   }
-
-
 
 
 
   render() {
     const emoji = require("emoji-dictionary");
 
-    const exampleFormat = CARD_DATA.cards.map((entry) => {
-      return <Card text={entry.text} emoji={emoji.getUnicode(`${entry.emoji}`)} />
+    const exampleFormat = this.state.cards.map((entry, i ) => {
+      return <Card key={i}  text={entry.card.text} emoji={emoji.getUnicode(`${entry.card.emoji}`)} />
     });
 
     return (
